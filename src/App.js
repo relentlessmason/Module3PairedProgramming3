2827b17964e0b3498ba72b18dfb983584d9fe3d6
Mason <astinch09@gmail.com>
2022-08-14 14:50:08 -0400

    undid whatever broke npm, working on making links work next

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import ContactUs from "./pages/ContactUs";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-
 <Router>
       <Header />
-   <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/contactus" component={ContactUs} />
-  
- </Switch>
+      <Home />
       <Footer />
- </Router> 
+</Router>
     </div>
   );
 }
